d0bb4549abab001303d096aa4c9202ad35ff006c
jdktomcat <jdktomcat@163.com>
2017-03-24 18:08:04 +0800

    对象工厂学习

---
+++ src/main/java/com/xdd/mdemo/pack/blog/plugin/ExamplePlugin.java
@@ -0,0 +1,25 @@
+package com.xdd.mdemo.pack.blog.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+/**
+ * Created by angelo.tang on 2017-3-24.
+ */
+@Intercepts({@Signature(type= Executor.class, method = "update", args = {MappedStatement.class,Object.class})})
+public class ExamplePlugin implements Interceptor {
+    public Object intercept(Invocation invocation) throws Throwable {
+        return invocation.proceed();
+    }
+
+    public Object plugin(Object target) {
+        return Plugin.wrap(target,this);
+    }
+
+    public void setProperties(Properties properties) {
+
+    }
+}
